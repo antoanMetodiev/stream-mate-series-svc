d38dfad2835a4d030dfa02f6bc485673fbe33d7d
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-04-02 19:12:34 +0300

    Update CorsConfig.java

--- src/main/java/com/example/streammateseriessvc/app/configs/CorsConfig.java
+++ src/main/java/com/example/streammateseriessvc/app/configs/CorsConfig.java
@@ -6,11 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
+  
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Позволява всички пътища
-                .allowedOrigins("https://dark-sissy-stream-mate-b1e9d2a2.koyeb.app", "http://localhost:8080")  // Разрешава само даден домейн
+                .allowedOrigins("https://married-miquela-stream-mate-org-original-ce114be1.koyeb.app", "http://localhost:8080")  // Разрешава само даден домейн
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // Разрешава само определени методи
                 .allowedHeaders("*");  // Позволява всички хедъри
     }
